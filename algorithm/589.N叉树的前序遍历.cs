769b9fd284b2b8df5a7035a59cc21bbd5d848883
子非鱼 <96179195+zfyzmh@users.noreply.github.com>
2024-02-18 15:59:52 +0800

    完成589N叉数的前序遍历

---
+++ algorithm/589.N叉树的前序遍历.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using SharpCompress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm
+{
+    /// <summary>
+    /// 给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+    /// </summary>
+    internal class _589
+    {
+        [Test]
+        public void Test1()
+        {
+            Node node = new(1);
+            node.children = new List<Node>() { new(3, [new(5), new(6)]), new(2), new(4) };
+
+            Console.WriteLine(string.Join(" ", Preorder(node)));
+        }
+
+        public IList<int> Preorder(Node root)
+        {
+            if (root == null) return new List<int>();
+
+            var result = new List<int>();
+
+            result.Add(root.val);
+
+            if (root.children != null && root.children.Any())
+            {
+                foreach (var child in root.children)
+                {
+                    result.AddRange(Preorder(child));
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node()
+        { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
